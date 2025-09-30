e05f21345b89611ab5b62e559ca5961318db8cc9
Andriambololona Faniloniaina Princy <prxandxa@gmail.com>
2025-09-30 12:21:04 +0300

    Organisation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import MainPage from './pages/MainPage.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/", Component: MainPage
+  }
+])
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
---
+++ client/src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+const MainPage = () => {
+
+  return (
+    <div className="grid place-items-center">
+      Hello World
+    </div>
+  )
+}
+
+export default MainPage
