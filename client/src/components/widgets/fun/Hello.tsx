90aac44d263c1449624c946cb25152fbc9d0a28d
Andriambololona Faniloniaina Princy <prxandxa@gmail.com>
2025-10-02 00:14:23 +0300

    Adding funny widget

---
+++ client/src/components/widgets/fun/Hello.tsx
@@ -0,0 +1,8 @@
+const Hello = () => {
+  return (
+    <div className="mx-auto text-3xl font-semibold tracking-wide my-10">
+      Hello 😉
+    </div>
+  )
+}
+export default Hello
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,11 +1,13 @@
 import SaleWidget from "../components/widgets/sale/SaleWidget.tsx";
 import MembershipWidget from "../components/widgets/membership/MembershipWidget.tsx";
+import Hello from "../components/widgets/fun/Hello.tsx";
 
 const MainPage = () => {
 
   return (
     <div className="h-full px-2 sm:px-0 pt-2 sm:pt-10">
       <div className="flex flex-col gap-10 pb-10">
+        <Hello/>
         <SaleWidget/>
         <MembershipWidget/>
       </div>
