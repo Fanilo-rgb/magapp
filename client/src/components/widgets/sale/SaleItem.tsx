d69c50263bc1caf80a4c422c5891686418d6beb9
Andriambololona Faniloniaina Princy <prxandxa@gmail.com>
2025-10-01 22:24:45 +0300

    Updating the UI

--- client/src/components/widgets/sale/SaleItem.tsx
+++ client/src/components/widgets/sale/SaleItem.tsx
@@ -27,28 +27,32 @@ const SaleItem = ({data}: Props) => {
     <div
       onMouseEnter={() => setIsOver(true)}
       onMouseLeave={() => setIsOver(false)}
-      className="h-8 sm:h-10 flex justify-between items-center cursor-pointer border shadow shadow-transparent hover:shadow-black/20 border-gray-300 p-1 sm:p-2 rounded-xl transition gap-2"
+      className="relative"
     >
-      {data.isCustomer && <span className="h-2 rounded-full ml-2 sm:ml-1 w-2 bg-cyan-400"/>}
-      <span className="truncate flex-1 pl-1">{data.name}</span>
-      <span className="verticalDivider"/>
-      <div className="flex gap-2 items-center">
-        <PillBottle size={16}/>
-        <span className="w-4 text-center">{data.products.quantity}</span>
-      </div>
-      <span className="verticalDivider"/>
-      <div className="flex gap-2 items-center">
-        <CircleDollarSign size={16}/>
-        <span className="w-8 text-center">{data.products.bv}</span>
-      </div>
-      <span className="verticalDivider"/>
-      <div className="flex gap-2 items-center">
-        <Banknote size={16}/>
-        <span className="hidden md:block w-20 text-center">{data.products.totalPrice.toLocaleString()}</span>
-        <span className="block md:hidden w-10 text-center">{formatCurrency(data.products.totalPrice)}</span>
+      <div
+        className="h-8 sm:h-10 flex justify-between items-center cursor-pointer border shadow shadow-transparent hover:shadow-black/20 border-gray-300 p-1 sm:p-2 rounded-xl transition gap-2"
+      >
+        {data.isCustomer && <span className="h-2 rounded-full ml-2 sm:ml-1 w-2 bg-cyan-400"/>}
+        <span className="truncate flex-1 pl-1">{data.name}</span>
+        <span className="verticalDivider"/>
+        <div className="flex gap-2 items-center">
+          <PillBottle size={16}/>
+          <span className="w-4 text-center">{data.products.quantity}</span>
+        </div>
+        <span className="verticalDivider"/>
+        <div className="flex gap-2 items-center">
+          <CircleDollarSign size={16}/>
+          <span className="w-8 text-center">{data.products.bv}</span>
+        </div>
+        <span className="verticalDivider"/>
+        <div className="flex gap-2 items-center">
+          <Banknote size={16}/>
+          <span className="hidden md:block w-20 text-center">{data.products.totalPrice.toLocaleString()}</span>
+          <span className="block md:hidden w-10 text-center">{formatCurrency(data.products.totalPrice)}</span>
+        </div>
       </div>
       {isOver && (
-        <div className="flex gap-2">
+        <div className="z-10 absolute flex justify-end gap-2 bg-white top-1/2 -translate-y-1/2 w-20 right-1 sm:right-2">
           {isMid && <IconButton icon={Printer}/>}
           <IconButton icon={Ellipsis}/>
         </div>
