19c7ec7da60bea3e62afcd27713f272395b8ba1a
Andriambololona Faniloniaina Princy <prxandxa@gmail.com>
2025-10-03 17:36:18 +0300

    New features

--- client/src/components/view/SaleView.tsx
+++ client/src/components/view/SaleView.tsx
@@ -1,6 +1,6 @@
 import {useSearchParams} from "react-router-dom";
 import ChoicesList from "../ui/inputs/ChoicesList.tsx";
-import {HandCoins, NotebookPen} from "lucide-react";
+import {HandCoins, NotebookPen, UserRoundCog} from "lucide-react";
 import type {Label} from "../../lib/types/types.ts";
 
 const SaleView = () => {
@@ -120,14 +120,132 @@ const DistributorView = () => {
           </div>
         </div>
       </div>
+
+      <div className="text-lg mt-2">
+        <p>Total: <span className="font-bold">453 600 ar</span></p>
+        <p>Bv: <span className="font-bold">240 $</span></p>
+      </div>
     </>
   )
 }
 
 const CustomerView = () => {
+
+  const personChoices: Label[] = [{ value: "customer", placeholder: "Client", color: "blue" },]
+
+  const saleChoices: Label[] = [{ value: "sale", placeholder: "Achat", color: "pink" }]
+
+  const paymentChoices: Label[] = [{ value: "telma", placeholder: "Mvola", color: "yellow" }]
+
   return (
-    <div>
-      Customer
-    </div>
+    <>
+      <div className="flex justify-between">
+        <span className="font-semibold">#00001</span>
+        <span className="text-gray-500">Il y a 2h</span>
+      </div>
+
+      <div>
+        <p className="w-full text-lg font-semibold py-1">
+          Ranarivony Felana
+        </p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <ChoicesList
+          icon={UserRoundCog}
+          label="Type de personne"
+          choices={personChoices}
+          disable
+        />
+        <ChoicesList
+          icon={NotebookPen}
+          label="Type de vente"
+          choices={saleChoices}
+          disable
+        />
+        <ChoicesList
+          icon={HandCoins}
+          choices={paymentChoices}
+          label="Type de payement"
+          disable
+        />
+      </div>
+
+      <div className="bg-cyan-200/50 text-cyan-800 p-2 rounded">
+        <div className="text-md flex gap-2 items-center">
+          <span className="w-24">18 00 80 80</span>
+          <span className="verticalDivider"/>
+          <span className="truncate">Andriambololona Faniloniaina Princy</span>
+        </div>
+      </div>
+
+      <hr/>
+
+      <div className="flex flex-col gap-2">
+        <p className="text-xs text-gray-500 pl-2">Achat</p>
+        <div className="flex flex-col gap-1">
+          <div className="flex justify-between items-center">
+            <span className="truncate">Kuding</span>
+            <input
+              type="number"
+              className="w-12 py-1 text-center outline-0"
+              value={1}
+              readOnly
+            />
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex">
+              <div className="w-12 text-center">12 $</div>
+              <div className="w-28 text-center">43 200 ar</div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="flex justify-between items-center">
+            <span className="truncate">Kuding</span>
+            <input
+              type="number"
+              className="w-12 py-1 text-center outline-0"
+              value={1}
+              readOnly
+            />
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex">
+              <div className="w-12 text-center">12 $</div>
+              <div className="w-28 text-center">43 200 ar</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr/>
+
+      <div className="flex flex-col gap-2">
+        <p className="text-xs text-gray-500 pl-2">Commande</p>
+        <div className="flex flex-col gap-1">
+          <div className="flex justify-between items-center">
+            <span className="truncate">Kuding</span>
+            <input
+              type="number"
+              className="w-12 py-1 text-center outline-0"
+              value={1}
+              readOnly
+            />
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex">
+              <div className="w-12 text-center">12 $</div>
+              <div className="w-28 text-center">43 200 ar</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-lg mt-2">
+        <p className="text-base">Total: <span className="font-semibold">453 600 ar</span></p>
+        <p className="text-base">Bv: <span className="font-semibold">240 $</span></p>
+        <p>20%: <span className="font-bold">75 300 ar</span></p>
+      </div>
+    </>
   )
 }
