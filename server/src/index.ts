99bb0b37857ffdb2d8f2002a44e5d66fd0a7921a
Andriambololona Faniloniaina Princy <prxandxa@gmail.com>
2025-10-11 02:26:25 +0300

    Add Axios integration for product fetching from FastAPI

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 import { NODE_ENV, PORT } from "./constants/env"
 import connectToDatabase from "./config/db";
@@ -14,6 +15,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello, World!" });
+})
+
+const fastapi = axios.create({
+  baseURL: 'http://localhost:8000/',
+})
+
+app.get('/api/v1/products', async (req, res) => {
+  const response = await fastapi.get('/products')
+
+  if (!response) return res.status(500).send({ success: false, message: 'Error fetching products' })
+
+  return res.status(200).send(response.data)
 })
 
 app.listen(PORT, async () => {
